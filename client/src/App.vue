32a9774cf1589567728d99d4515a80808bd54545
TimeCrunch101 <apaallen101@gmail.com>
2024-08-12 11:05:02 -0400

    pick a name for a user

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,25 @@
 <script setup>
+import {ref} from 'vue'
 import {useRouter} from "vue-router"
 const router = useRouter()
 
+const myName = ref("")
+
 router.push('/')
+
+const pickName = () => {
+    console.log(myName.value)
+
+}
 
 </script>
 
 <template>
+    <form @submit.prevent="pickName()">
+        <label>Pick Name: </label>
+        <input type="text" v-model="myName">
+        <button type="submit">Save</button>
+    </form>
     <RouterView />
 </template>
 
