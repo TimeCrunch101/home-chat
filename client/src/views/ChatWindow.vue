b219c7b607d1a653963056061d633cf6045aaf0c
TimeCrunch101 <apaallen101@gmail.com>
2024-08-11 22:30:19 -0400

    added rooms to FE

--- client/src/views/ChatWindow.vue
+++ client/src/views/ChatWindow.vue
@@ -5,19 +5,25 @@ import ChatInput from '../components/ChatInput.vue';
 import { io } from "socket.io-client";
 
 const socket = io("http://localhost:3000");
+const chats = ref([])
+const connectedRoom = ref("")
+const room = ref({
+    roomName: ""
+})
 
-const sendMsg = (chat) => {
-    socket.emit("send-msg", chat)
-}
-
-socket.on("server-send-msg", (data) => {
+socket.on("room-emit", (data) => {
     chats.value.push({
         string: data,
         isSelf: false
     })
 })
 
-const chats = ref([])
+const sendMsg = (chat) => {
+    socket.emit("room-msg", {
+        msg: chat,
+        room: connectedRoom.value
+    })
+}
 
 const pushMessage = (chat) =>{
     chats.value.push({
@@ -28,10 +34,19 @@ const pushMessage = (chat) =>{
 }
 
 
+const joinRoom = () => {
+    console.log("Join Room: ",room.value.roomName)
+    connectedRoom.value = room.value.roomName
+    socket.emit("join-room", room.value.roomName)
+}
 
 </script>
 
 <template>
+    <form @submit.prevent="joinRoom()">
+        <input type="text" v-model="room.roomName">
+        <button type="submit">Join room</button>
+    </form>
     <div class="chats-container">
         <div v-for="data in chats">
             <ChatBubble :chatValue="data.string" :isSelf="data.isSelf"/>
