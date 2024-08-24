3cc7cc352c70b496c26272fca59baefcedd6a839
timecrunch101 <apaallen101@gmail.com>
2024-08-24 12:56:44 -0400

    fix: for staging env

--- server/main.js
+++ server/main.js
@@ -2,12 +2,12 @@ if (process.env.NODE_ENV !== "production") require('dotenv').config()
 console.info(process.env.NODE_ENV)
 const express = require("express")
 const app = express()
-require("./controllers/socketController")
 const initGetRouter = require("./router/getRouter")
 const initPostRouter = require("./router/postRouter")
 initGetRouter(app)
 initPostRouter(app)
 
 if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "development") {
+  require("./controllers/socketController")
   app.listen(8080, console.log("API: http://localhost:8080"))
 }
